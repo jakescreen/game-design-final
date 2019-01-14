c708d2ef0b59ec808a59fc5aad3e23e169522ddb
jakescreen <36495369+jakescreen@users.noreply.github.com>
2019-01-14 21:21:06 +1300

    kjld;
    1.0

---
+++ Assets/scripts/clickButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class clickButton : MonoBehaviour
+{
+
+    public Text text = null;
+    public Button ruin;
+    public Button fix;
+
+    public void changeText(string word)
+    {
+        text.text = word;
+    }
+
+    public void makeUnclickable(Button but)
+    {
+        but.interactable = false;
+    }
+}
