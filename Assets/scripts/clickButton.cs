aa3e9db87cec61602d219c54f36df572409bf036
jakescreen <36495369+jakescreen@users.noreply.github.com>
2019-01-16 20:22:32 +1300

    almost almost there

--- Assets/scripts/clickButton.cs
+++ Assets/scripts/clickButton.cs
@@ -10,6 +10,9 @@ public class clickButton : MonoBehaviour
     public Text text = null;
     public Button ruin;
     public Button fix;
+    public Toggle t1, t2, t3, vanilla, chocolate, redVelvet, red, blue, green, purple, white, small, medium, large, circle, rect, star, icing
+        , writing, sprinkles, candles;
+    public static int correct = 0;
 
     public void changeText(string word)
     {
@@ -22,6 +25,38 @@ public class clickButton : MonoBehaviour
     }
     public void changeToEnd()
     {
+        if(t3.isOn == false)
+        {
+            correct += 1;
+        }
+        if (vanilla.isOn == false)
+        {
+            correct += 1;
+        }
+        if (blue.isOn == false)
+        {
+            correct += 1;
+        }
+        if (medium.isOn == false)
+        {
+            correct += 1;
+        }
+        if (icing.isOn == false)
+        {
+            correct += 1;
+        }
+        if (writing.isOn == false)
+        {
+            correct += 1;
+        }
+        if (candles.isOn == false)
+        {
+            correct += 1;
+        }
+        if (rect.isOn == false)
+        {
+            correct += 1;
+        }
         Application.LoadLevel("EndScreen");
     }
     public void makeUnclickable(Button but)
