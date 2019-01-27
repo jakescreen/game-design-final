276ea03f3fc14686bfac3116e292e9d4d2d5ab5a
jennyb1 <46670398+jennyb1@users.noreply.github.com>
2019-01-27 02:46:00 -0500

    score is a percentage

--- Assets/scripts/FinalScore.cs
+++ Assets/scripts/FinalScore.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Diagnostics;
 
 public class FinalScore : MonoBehaviour
 {
@@ -10,8 +11,9 @@ public class FinalScore : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //float points = clickButton.ans / 8;
-        score.text = "total: " + clickButton.ans.ToString() + "/8";
+        float points = (((float) clickButton.ans) / 8.0f) * 100.0f;
+        System.Console.WriteLine(points);
+        score.text = "total: " + points.ToString() + "%";
     }
 
     // Update is called once per frame
